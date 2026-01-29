edda494d7799c66b6b1ec8ddc1c8163b75fd8b27
Jadon Smith <1tenletters0@gmail.com>
2026-01-29 04:50:49 -0800

    :sparkles: setup eslint / lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import antfu from "@antfu/eslint-config";
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  antfu({
+    type: "app",
+    vue: true,
+    typescript: true,
+    formatters: false,
+
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  }, {
+    rules: {
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "unicorn/filename-case": ["error", {
+        case: "camelCase",
+        ignore: ["README.md"],
+      }],
+    },
+  }),
+);
