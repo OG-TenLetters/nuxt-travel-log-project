e2104ddfbc9fb37ba255bb0533480ecccc4e39b0
OG-TenLetters <1tenletts0@gmail.com>
2026-02-05 06:32:33 -0800

    Hopefully this fixes the Auth Errors for github Oauth

--- lib/env.ts
+++ lib/env.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
-
 import tryParseEnv from "./try-parse-env";
+
+import "dotenv/config";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
+  AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
