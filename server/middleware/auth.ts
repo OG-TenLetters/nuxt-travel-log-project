c3ad6178b92c2b3543588cc8d2202094481ee326
Jadon Smith <1tenletters0@gmail.com>
2026-02-06 10:15:03 -0800

    Fixed auth button routing when user is one / instead of /dashboard (#61)
    Co-authored-by: OG-TenLetters <1tenletts0@gmail.com>

---
+++ server/middleware/auth.ts
@@ -0,0 +1,14 @@
+import { getAuth } from "~~/lib/auth";
+
+const auth = getAuth();
+
+export default defineEventHandler(async (event) => {
+  if (event.path.startsWith("/dashboard")) {
+    const session = await auth.api.getSession({
+      headers: event.headers,
+    });
+    if (!session) {
+      await sendRedirect(event, "/", 302);
+    }
+  }
+});
